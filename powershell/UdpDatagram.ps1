0501a2511eacdf06d03907612eb8ab82aacfd720
ps-przemekaugustyn <przemek.augustyn@pricespider.com>
2020-08-10 23:52:58 -0700

    feat: add loop to Send-UdpDatagram to test delivery completion

--- powershell/UdpDatagram.ps1
+++ powershell/UdpDatagram.ps1
@@ -1,22 +1,33 @@
 Write-Host "Installing UdpDatagram functions..."
 
+# Send-UdpDatagram -EndPoint "10.21.101.174" -Port 30000 -Message "test_measurement,tagname=tagvalue,influxdb_database=spu executiontime=123" -includeCounter 1 -count 500
 function Send-UdpDatagram
 {
       Param ([string] $EndPoint, 
       [int] $Port, 
-      [string] $Message)
+      [string] $Message,
+      [bool] $includeCounter,
+      [int] $count=1)
 
       try {
             $IP = [System.Net.Dns]::GetHostAddresses($EndPoint) 
-            $Address = [System.Net.IPAddress]::Parse($IP) 
-
+            $Address = [System.Net.IPAddress]::Parse($IP)
             $EndPoints = New-Object System.Net.IPEndPoint($Address, $Port) 
             Write-Host "sending datagram to $EndPoints"
        
             $Socket = New-Object System.Net.Sockets.UDPClient 
-            $EncodedText = [Text.Encoding]::ASCII.GetBytes($Message) 
-            $Sent=$Socket.Send($EncodedText, $EncodedText.Length, $EndPoints)
-            Write-Host "sent $Sent bytes"
+
+            for($i=0;$i -lt $count;$i++){
+                  if($includeCounter){
+                        $datagram="$Message,counter=$i"
+                  }
+                  else{
+                        $datagram=$Message
+                  }
+                  $EncodedText = [Text.Encoding]::ASCII.GetBytes("$datagram")
+                  $Sent=$Socket.Send($EncodedText, $EncodedText.Length, $EndPoints)
+                  Write-Host "sent $Sent bytes"
+            }
             $Socket.Close() 
       }
       catch {
